b510ccbc690d60c153cc1510c799716641fd6e99
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-04-17 15:44:18 +0100

    moved symlinked yacreader library to different branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,40 +1,17 @@
 #!/bin/sh
 
 comicsDir="/comics/.yacreaderlibrary"
-configDir="/config/.yacreaderlibrary"
-
-if [ -d "$configDir" ]; then
-  echo "${configDir} already exists"
-else
-  echo "making directory: ${configDir}"
-  mkdir "$configDir"
-fi
 
 if [ -d "$comicsDir" ]; then
-  if [ -L "$comicsDir" ]; then
-    echo "symbolic link to "$configDir" found."cd /
-    if [ "$(ls -A $comicsDir)" ]; then
-      echo "$comicsDir is not empty. adding existing library to YACReaderLibraryServer"
-      YACReaderLibraryServer add-library Comics /comics
-    else
-      echo "$comicsDir is empty. Creating library"
-      YACReaderLibraryServer create-library Comics /comics
-    fi
-  else
-    echo "existing .yacreaderlibrary directory found"
-    echo "copying "$comicsDir" to "$configDir""
-    cp -R "$comicsDir"/* "$configDir"
-    currentTime=$(date +%s)
-    echo "renaming old "$comicsDir" to "$configDir.$currentTime""
-    mv "$comicsDir" "$comicsDir.$currentTime"
-    echo "creating symbolic link to "$configDir""
-    ln -s "$configDir" "$comicsDir"
-    echo "adding existing library to YACReaderLibraryServer"
+  if [ "$(ls -A $comicsDir)" ]; then
+    echo "$comicsDir is not empty. Adding existing library to YACReaderLibraryServer"
     YACReaderLibraryServer add-library Comics /comics
+  else
+    echo "$comicsDir is empty. Creating library"
+    YACReaderLibraryServer create-library Comics /comics
   fi
 else
-  echo "Creating library"
-  ln -s "$configDir" "$comicsDir"
+  echo "$comicsDir not found. Creating library"
   YACReaderLibraryServer create-library Comics /comics
 fi
 
